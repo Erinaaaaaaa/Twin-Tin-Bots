fc25bb4c215107d7d955dd8b8f110deae597adbb
Rongead <sebastien.prunier@etu.univ-lehavre.fr>
2019-06-17 10:47:27 +0200

    Update Plateau, Joueur

---
+++ Joueur.java
@@ -0,0 +1,11 @@
+public class Joueur
+{
+	private int[] pos;
+
+	public Joueur(int ligne, int colonne)
+	{
+		this.pos = {ligne, colonne};
+	}
+
+	public int[] getPos() {return pos;}
+}
--- Plateau.java
+++ Plateau.java
@@ -8,4 +8,36 @@ public class Plateau
 	{
 
 	}
+
+	public boolean isNextFree(Joueur joueur, char dir)
+	{
+		int[] pos = joueur.getPos();
+		pos = nextPos(pos, dir);
+		for(Joueur j : tabJoueurs)
+		{
+			if(j != joueur && j.getPos() == pos)
+				return false;
+		}
+
+		return true;
+	}
+
+	private int[] nextPos(int[] initPos, char dir)
+	{
+		char[] tabDir = {'N', 'O', 'S', 'E'};
+		int[] pos = initPos;
+		for(int i = 0; i < tabDir.length; i++)
+		{
+			if(dir == tabDir[i])
+			{
+				if(i < 2)
+					pos[i]--;
+
+				if(i >= 2)
+					pos[i % 2]++;
+			}
+		}
+
+		return pos;
+	}
 }
