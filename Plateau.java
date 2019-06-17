0910e144e0705b294e97c75394d08000bb52ce5d
Rongead <sebastien.prunier@etu.univ-lehavre.fr>
2019-06-17 15:56:09 +0200

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -34,7 +34,7 @@ public class Plateau
 			}
 		}
 
-		r = new Robot(NB_LIGNES/2, NB_COLONNES/2, 6);
+		r = new Robot(NB_LIGNES/2, NB_COLONNES/2, 5);
 		jouer();
 	}
 
@@ -91,7 +91,7 @@ public class Plateau
 		int[] pos = Arrays.copyOf(initPos, initPos.length);
 		switch(dir)
 		{
-			case 1 :
+			case 0 :
 				if(pos[0] % 2 == 0)
 				{
 					pos[0]--;
@@ -102,7 +102,7 @@ public class Plateau
 					pos[0]--;
 				}
 				break;
-			case 2 :
+			case 1 :
 				if(pos[0] % 2 == 0)
 				{
 					pos[0]--;
@@ -113,24 +113,24 @@ public class Plateau
 					pos[1]++;
 				}
 				break;
-			case 3 :
+			case 2 :
 				pos[1]++;
+				break;
+			case 3 :
+				if(pos[0] % 2 == 0)
+				{
+					pos[0]++;
+				}
+				else
+				{
+					pos[0]++;
+					pos[1]++;
+				}
 				break;
 			case 4 :
 				if(pos[0] % 2 == 0)
 				{
 					pos[0]++;
-				}
-				else
-				{
-					pos[0]++;
-					pos[1]++;
-				}
-				break;
-			case 5 :
-				if(pos[0] % 2 == 0)
-				{
-					pos[0]++;
 					pos[1]--;
 				}
 				else
@@ -138,7 +138,7 @@ public class Plateau
 					pos[0]--;
 				}
 				break;
-			case 6 :
+			case 5 :
 				pos[1]--;
 				break;
 		}
