d62d9982505d6fabebe87c03e2924ca602afb735
Rongead <sebastien.prunier@etu.univ-lehavre.fr>
2019-06-20 11:06:13 +0200

    Update oui

--- ttb/metier/Plateau.java
+++ ttb/metier/Plateau.java
@@ -186,7 +186,10 @@ public class Plateau
 		if(t == Tuile.BASE || (t == Tuile.ROBOT && bOk) || (t == Tuile.VIDE))
 		{
 			if(t == Tuile.BASE)
+			{
 				getJoueurParBase(next).addCristal(cristal);
+				listeAttente();
+			}
 
 			if(t == Tuile.VIDE)
 				plateau[next[0]][next[1]] = cristal;
@@ -195,16 +198,6 @@ public class Plateau
 		}
 
 		r.chargerCrystal(cristal);
-	}
-
-	public boolean aPlusDeCristaux()
-	{
-		for(int i = 0; i < plateau.length; i++)
-			for(int j = 0; j < plateau[i].length; j++)
-				if(Tuile.isCristal(plateau[i][j]))
-					return false;
-
-		return true;
 	}
 
 	public void listeAttente()
